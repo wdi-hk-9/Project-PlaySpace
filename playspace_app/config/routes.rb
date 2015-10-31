c44c0da6b07044c597ef98ccdc495bd4be7a00fb
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-01 00:02:30 +0800

    updated root route for landing page

--- playspace_app/config/routes.rb
+++ playspace_app/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :static_pages, only: [:about, :index]
-  get '/playspace', to: 'static_pages#home'
+  get '/', to: 'static_pages#home'
 
-  get '/playspace/about', to: 'static_pages#about'
+  get '/about', to: 'static_pages#about'
 
   root "users#index"
   resources :users, only: [:new, :index, :create]
