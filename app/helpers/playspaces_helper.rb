5c85ca3711312342fdb4263e8bdb7262d069fea7
adaau <ada.au.cheng@gmail.com>
2015-11-03 20:59:03 +0800

    minor changes before putting features in playspaces helper

--- app/helpers/playspaces_helper.rb
+++ app/helpers/playspaces_helper.rb
@@ -1,2 +1,9 @@
 module PlayspacesHelper
+  # def features_list(playspace)
+  #   array = []
+  #   ["bike_path", "merry_go_round", "play_structure", "seesaw", "slide", "swing", "carpark", "snack_shop" "wc"].each do |feature|
+  #     array.push feature.titleize if playspace[feature]
+  #   end
+  #   array.to_sentence
+  # end
 end
