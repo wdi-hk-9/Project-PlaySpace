97f2dc44f339f5c4bfddc8d4b00f2f2009dd0d25
adaau <ada.au.cheng@gmail.com>
2015-11-03 22:17:26 +0800

    added features_list to playspaces_helper; styled reviews page

--- app/helpers/playspaces_helper.rb
+++ app/helpers/playspaces_helper.rb
@@ -1,9 +1,10 @@
 module PlayspacesHelper
-  # def features_list(playspace)
-  #   array = []
-  #   ["bike_path", "merry_go_round", "play_structure", "seesaw", "slide", "swing", "carpark", "snack_shop" "wc"].each do |feature|
-  #     array.push feature.titleize if playspace[feature]
-  #   end
-  #   array.to_sentence
-  # end
+  def features_list(playspace)
+    @features = ["bike_path", "merry_go_round", "play_structure", "seesaw", "slide", "swing", "carpark", "snack_shop" "wc"]
+    array = []
+    @features.each do |feature|
+      array.push feature.titleize if playspace[feature]
+    end
+    array.to_sentence
+  end
 end
