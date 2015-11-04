2409b081993987e20b11b8ada045a19889facc38
Denis Cheung <d.s.h.cheung@gmail.com>
2015-11-04 23:51:54 +0800

    format for key in hash

--- app/helpers/playspaces_helper.rb
+++ app/helpers/playspaces_helper.rb
@@ -1,15 +1,15 @@
 module PlayspacesHelper
   def features_list(playspace)
     @features = {
-      "bike_path" => "bike.png",
-      "merry_go_round" => "merrygoround.png",
-      "play_structure" => "playstructure.png",
-      "seesaw" => "seesaw.png",
-      "slide" => "slide.png",
-      "swing" => "swing.png",
-      "carpark" => "carpark.png",
-      "snack_shop" => "snack.png",
-      "wc" => "wc.png"
+      bike_path: "bike.png",
+      merry_go_round: "merrygoround.png",
+      play_structure: "playstructure.png",
+      seesaw: "seesaw.png",
+      slide: "slide.png",
+      swing: "swing.png",
+      carpark: "carpark.png",
+      snack_shop: "snack.png",
+      wc: "wc.png"
     }
     @array = []
     @features.each do |feature, image|
