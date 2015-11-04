23f51166f9000601efa5b75da16ebe10de1437b9
adaau <ada.au.cheng@gmail.com>
2015-11-04 08:30:38 +0800

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,13 @@ playspace_list = [
   ["Mount Austin Playground", "Mount Austin Road", "The Peak", "HK", nil, nil, true, nil, true, true, nil, nil, true, 3],
   ["Sai On Lane Playground", "Sai On Lane", "Sai Wan", "HK", nil, nil, true, nil, true, nil, nil, nil, nil, 3],
   ["Victoria Park", "1 Hing Fat Street", "Causeway Bay", "Hong Kong", nil, nil, true, nil, true, true, true, true, true, 2],
-  ["Quarry Bay Park", "Taikoo Shing", "Taikoo Shing", "HK", nil, nil, true, true, true, true, nil, nil, true, 3]
+  ["Quarry Bay Park", "Taikoo Shing", "Taikoo Shing", "HK", nil, nil, true, true, true, true, nil, nil, true, 3],
+  ["Ap Lei Chau Park", "Ap Lei Chau Main Street", "Aberdeen", "HK", nil, nil, true, nil, true, true, nil, nil, true, 3],
+  ["Repulse Bay Beach", "16 Beach Road", "Repulse Bay", "HK", nil, true, true, true, true, nil, true, true, true, 1],
+  ["Wan Chai Park", "Cross Lane", "Wan Chai", "HK", nil, nil, true, true, true, nil, nil, nil, nil, 3],
+  ["Ho Man Tin Park", "1 Chung Yee Street", "Ho Man Tin", "Kln", nil, nil, true, true, true, nil, nil, nil, nil, 3],
+  ["To Kwa Wan Recreation Ground", "66 Ha Heung Road", "To Kwa Wan", "Kln", nil, nil, true, nil, true, true, nil, nil, nil, 3],
+  ["Yuet Wah Street Playground", "Yuet Wah Street", "Kwun Tong", "Kln", nil, nil, true, nil, true, nil, nil, nil, nil, 3]
 ]
 
 playspace_list.each do |name, address, district, region, bike_path, merry_go_round, play_structure, seesaw, slide, swing, carpark, snack_shop, wc, category_id|
