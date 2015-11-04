daefe1212941e88db362f858ec64ab219c455b98
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-04 14:15:33 +0800

    filter playspace feature added

--- app/assets/javascripts/playspaces.js
+++ app/assets/javascripts/playspaces.js
@@ -7,27 +7,35 @@ var PlayspaceSidebar = function () {
 $(document).ready(function(){
   var option = {
     HK: ["Central", "North Point", "Repulse Bay", "The Peak", "Wan Chai", "Western"],
-    KLN: ["Ho Man Tin", "Tsim Sha Tsui", "Wong Tai Sin"],
+    Kln: ["Ho Man Tin", "Tsim Sha Tsui", "Wong Tai Sin"],
     NT: ["Sai Kung", "Sha Tin", "Tai Po"]
   }
 
-  $("#playspace_region").on('change', function() {
-    $("#playspace_district").html("");
-    if ($("#playspace_region").val() == "HK") {
-      for (var i = 0; i < option["HK"].length; i++) {
-        $("#playspace_district").append("<option value='"+option["HK"][i]+"'>"+option["HK"][i]+"</option>")
-      }
-    }
-    else if ($("#playspace_region").val() == "Kln") {
-      for (var i = 0; i < option["Kln"].length; i++) {
-        $("#playspace_district").append("<option value='"+option["Kln"][i]+"'>"+option["Kln"][i]+"</option>")
-      }
-    }
-    else {
-      for (var i = 0; i < option["NT"].length; i++) {
-        $("#playspace_district").append("<option value='"+option["NT"][i]+"'>"+option["NT"][i]+"</option>")
-      }
-    }
+  var bindRegionDistrict = function (formID) {
+    $(formID + " #playspace_region").on('change', function() {
+      $(formID + " #playspace_district").html("");
+      $(formID + " #playspace_district").append("<option value>Select by district:</option>")
 
-  })
+      if ($(formID + " #playspace_region").val() == "HK") {
+        for (var i = 0; i < option["HK"].length; i++) {
+          $(formID + " #playspace_district").append("<option value='"+option["HK"][i]+"'>"+option["HK"][i]+"</option>")
+        }
+      }
+      else if ($(formID + " #playspace_region").val() == "Kln") {
+        for (var i = 0; i < option["Kln"].length; i++) {
+          $(formID + " #playspace_district").append("<option value='"+option["Kln"][i]+"'>"+option["Kln"][i]+"</option>")
+        }
+      }
+      else {
+        for (var i = 0; i < option["NT"].length; i++) {
+          $(formID + " #playspace_district").append("<option value='"+option["NT"][i]+"'>"+option["NT"][i]+"</option>")
+        }
+      }
+
+    })
+  };
+
+  bindRegionDistrict('#search')
+  bindRegionDistrict('#new_playspace')
+
 })
--- app/models/playspace.rb
+++ app/models/playspace.rb
@@ -1,4 +1,21 @@
 class Playspace < ActiveRecord::Base
   belongs_to :category
   has_many :reviews
+
+
+  before_validation :format_values
+
+  validates :name, presence: true
+  validates :region, presence: true
+  validates :district, presence: true
+  validates :category, presence: true
+
+  def format_values
+    self[:name] = self[:name].downcase if self[:name].present?
+    self[:region] = self[:region].downcase if self[:region].present?
+    self[:district] = self[:district].downcase if self[:district].present?
+    self[:category] = self[:category].downcase if self[:category].present?
+
+  end
+
 end
