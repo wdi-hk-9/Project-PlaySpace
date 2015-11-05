886c26c8b3a43021f5fa07cef98ff9c721f6de71
adaau <ada.au.cheng@gmail.com>
2015-11-05 14:42:34 +0800

    added Fer's validation code to bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,4 +4,17 @@ class Bookmark < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :playspace_id, presence: true, uniqueness: true
+
+  validate :relationship_uniqueness
+
+  def relationship_uniqueness
+    user_id = current_user.id
+    playspace_id = params[:bookmark]
+    existing_record = Bookmark.find(:first, :conditions => ["user_id = ? AND playspace_id = ?", user_id, playspace_id])
+    unless existing_record.blank?
+      errors.add(:user_id, "has already been saved for this relationship")
+      errors.add(:playspace_id, "has already been saved for this relationship")
+    end
+  end
+
 end
