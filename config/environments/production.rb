ad0bfeef2a05788e42e7f364f8cab2699540bdbe
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-05 15:38:46 +0800

    Added right AWS S3 bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'rails-instagram-dev',
+      :bucket => 'playspaces-wdi',
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
