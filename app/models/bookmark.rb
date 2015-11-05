98bc896909e92368db511588bba6979d19f03357
adaau <ada.au.cheng@gmail.com>
2015-11-05 12:57:49 +0800

    fixed per Fer comments

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,5 @@ class Bookmark < ActiveRecord::Base
   belongs_to :playspace
 
   validates :user_id, presence: true
-  validates :playspace_id, presence: true
-  validates_uniqueness_of :user_id, scope: [:playspace_id]
-  validates_uniqueness_of :playspace_id, scope: [:user_id]
+  validates :playspace_id, presence: true, uniqueness: true
 end
